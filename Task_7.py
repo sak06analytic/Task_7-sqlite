4283cc408016682e813d86d8cce065883549ab66
Sakshi <khedekarsakshi948@gmail.com>
2025-04-17 23:18:56 +0530

    Add files via upload

---
+++ Task_7.py
@@ -0,0 +1,34 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======== RESTART: C:/Users/saksh/OneDrive/Desktop/.database/load_sql.py ========
+✅ Database file FOUND!
+📋 Tables in DB: [('superstore',)]
+
+✅ Query successful! Here's your data:
+
+        product  total_qty   revenue
+0        Mobile       19.0  210045.0
+1  Refrigerator       10.0  119000.0
+2         Sugar      160.0  336000.0
+3        Tablet        7.0  168994.0
+4        Tshirt      200.0  240000.0
+>>> 
+======== RESTART: C:/Users/saksh/OneDrive/Desktop/.database/load_sql.py ========
+✅ Database file FOUND!
+📋 Tables in DB: [('superstore',)]
+
+✅ Query successful! Here's your data:
+
+        product  total_qty   revenue
+0        Mobile       19.0  210045.0
+1  Refrigerator       10.0  119000.0
+2         Sugar      160.0  336000.0
+3        Tablet        7.0  168994.0
+4        Tshirt      200.0  240000.0
+        product  total_qty   revenue
+0        Mobile       19.0  210045.0
+1  Refrigerator       10.0  119000.0
+2         Sugar      160.0  336000.0
+3        Tablet        7.0  168994.0
+4        Tshirt      200.0  240000.0
